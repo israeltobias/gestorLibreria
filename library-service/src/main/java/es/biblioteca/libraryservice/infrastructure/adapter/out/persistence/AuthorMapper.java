8288632527da29d81ffddeaea7d1db3b5ee56835
israel.tobias <25621386+israeltobias@users.noreply.github.com>
2026-04-28 12:14:08 +0200

    Modified mappers to added dto conversions

--- library-service/src/main/java/es/biblioteca/libraryservice/infrastructure/adapter/out/persistence/AuthorMapper.java
+++ library-service/src/main/java/es/biblioteca/libraryservice/infrastructure/adapter/out/persistence/AuthorMapper.java
@@ -1,6 +1,10 @@
 package es.biblioteca.libraryservice.infrastructure.adapter.out.persistence;
 
 import es.biblioteca.libraryservice.domain.model.Author;
+import es.biblioteca.libraryservice.infrastructure.dto.AuthorRefDto;
+import es.biblioteca.libraryservice.infrastructure.dto.AuthorResponseDto;
+
+import java.util.List;
 
 public class AuthorMapper {
 
@@ -12,10 +16,30 @@ public class AuthorMapper {
         return new Author(authorEntity.getId(), authorEntity.getName(), authorEntity.getSurname());
     }
 
-    public static AuthorEntity toEntitiy(Author author) {
+    public static AuthorEntity toEntity(Author author) {
         return new AuthorEntity(author.getName(), author.getSurname());
     }
 
 
-    ///DTOs
+    /// DTOs
+
+    public static AuthorRefDto domainToAuthorRefDto(Author author) {
+        return new AuthorRefDto(author.getName(), author.getSurname());
+    }
+
+    public static Author authorRefDtoToDomain(AuthorRefDto authorRefDto) {
+        return Author.builder().name(authorRefDto.name()).surname(authorRefDto.surname()).build();
+    }
+
+    public static AuthorResponseDto domainToAuthorResponseDTO(Author author) {
+        //ToDo: obtener su lista de libros
+        return new AuthorResponseDto(author.getName(), author.getSurname(), List.of());
+    }
+
+    public static Author authorResponseDTOToDomain(AuthorResponseDto authorResponseDto) {
+        return Author.builder()
+                .name(authorResponseDto.name())
+                .surname((authorResponseDto.surname()))
+                .build();
+    }
 }
--- library-service/src/main/java/es/biblioteca/libraryservice/infrastructure/adapter/out/persistence/BookMapper.java
+++ library-service/src/main/java/es/biblioteca/libraryservice/infrastructure/adapter/out/persistence/BookMapper.java
@@ -1,6 +1,8 @@
 package es.biblioteca.libraryservice.infrastructure.adapter.out.persistence;
 
 import es.biblioteca.libraryservice.domain.model.Book;
+import es.biblioteca.libraryservice.infrastructure.dto.BookRefDto;
+import es.biblioteca.libraryservice.infrastructure.dto.BookResponseDto;
 
 public class BookMapper {
 
@@ -22,5 +24,31 @@ public class BookMapper {
                 .build();
     }
 
-    ///DTOs
+    /// DTOs
+    public static BookRefDto domainToBookRefDto(Book book) {
+        return new BookRefDto(book.getName(), book.getIsbn(), book.getGenre());
+    }
+
+    public static Book bookRefDtoToDomain(BookRefDto bookRefDto) {
+        return Book.builder()
+                .name(bookRefDto.name())
+                .isbn(bookRefDto.isbn())
+                .genre(bookRefDto.genre())
+                .build();
+    }
+
+    public static BookResponseDto domainToBookResponseDTO(Book book) {
+        return new BookResponseDto(book.getName(), book.getIsbn(), book.getGenre(), book.getNumPages(),
+                AuthorMapper.domainToAuthorRefDto(AuthorMapper.toDomain(book.getAuthor())));
+    }
+
+    public static Book bookResponseDTOToDomain(BookResponseDto bookResponseDto) {
+        return Book.builder()
+                .name(bookResponseDto.name())
+                .isbn(bookResponseDto.isbn())
+                .genre(bookResponseDto.genre())
+                .numPages(bookResponseDto.numPages())
+                .author(AuthorMapper.toEntity(AuthorMapper.authorRefDtoToDomain(bookResponseDto.author())))
+                .build();
+    }
 }
